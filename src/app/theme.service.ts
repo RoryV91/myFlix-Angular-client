9baeefcf70247230516a2879dde41d3f2e3175b9
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2025-03-02 11:45:46 -0800

    Update theme.service.ts

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  private _theme = new BehaviorSubject<string>('light-theme');
+  currentTheme: Observable<string> = this._theme.asObservable();
 
   constructor() { }
+
+  setTheme(theme: string): void {
+    this._theme.next(theme);
+  }
 }
